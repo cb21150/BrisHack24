1624d5f15e21e8f520ad0b017a81c897f005e645
yisong1027 <1027881714@qq.com>
2024-02-25 21:46:48 +0000

    update NurseInput.js

--- nurse-input/src/page/NurseInput.js
+++ nurse-input/src/page/NurseInput.js
@@ -73,8 +73,11 @@ function NurseInput(props) {
                         <div className="sm:col-span-2 inline-block">
                             <label htmlFor="company"
                                    className="mt-1 float-left text-sm font-semibold leading-6 text-gray-900">Hydration States</label>
-                            <input type="text" name="company" id="company" autoComplete="organization"
-                                   className="float-right ml-8 w-96 rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                            <select
+                                className="float-right rounded border appearance-none border-gray-300 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-200 focus:border-indigo-500 text-base pl-3 pr-10">
+                                <option>Yes</option>
+                                <option>No</option>
+                            </select>
                         </div>
 
                         <div className="sm:col-span-2 inline-block">
@@ -87,8 +90,11 @@ function NurseInput(props) {
                         <div className="sm:col-span-2 inline-block">
                             <label htmlFor="company"
                                    className="mt-1 float-left text-sm font-semibold leading-6 text-gray-900">Alertness</label>
-                            <input type="text" name="company" id="company" autoComplete="organization"
-                                   className="float-right ml-8 w-96 rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                            <select
+                                className="float-right rounded border appearance-none border-gray-300 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-200 focus:border-indigo-500 text-base pl-3 pr-10">
+                                <option>Yes</option>
+                                <option>No</option>
+                            </select>
                         </div>
 
                         <div className="sm:col-span-2 inline-block">
