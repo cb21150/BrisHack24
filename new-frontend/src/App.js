b8794d3e36018fff2529b3fabdb37ef2713bd417
mokshpatel <oc22442@bristol.ac.uk>
2024-02-25 20:51:56 +0000

    Nurse side form change

--- new-frontend/src/App.js
+++ new-frontend/src/App.js
@@ -2,25 +2,31 @@ import logo from "./logo.svg";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import LoginScreen from "./LoginScreen";
 import PatientInfo from "./PatientInfo";
+import { Nurse } from "./Nurse";
 import React, { useState } from "react";
 import "./App.css";
 
 function App() {
   return (
     <BrowserRouter>
-    <nav className="bg-gray-800 p-4">
-                <ul className="flex space-x-4 text-white">
-                    <li>
-                        <Link to="/" className="hover:underline">Patient Information</Link>
-                    </li>
-                    <li>
-                        <Link to="/login" className="hover:underline">Patient Health Condition</Link>
-                    </li>
-                </ul>
-            </nav>
+      <nav className="bg-gray-800 p-4">
+        <ul className="flex space-x-4 text-white">
+          <li>
+            <Link to="/" className="hover:underline">
+              Patient Information
+            </Link>
+          </li>
+          <li>
+            <Link to="/login" className="hover:underline">
+              Patient Health Condition
+            </Link>
+          </li>
+        </ul>
+      </nav>
       <Routes>
         <Route path="/" element={<PatientInfo />} />
         <Route path="/login" element={<LoginScreen />} />
+        <Route path="/nurse" element={<Nurse />} />
       </Routes>
     </BrowserRouter>
   );
