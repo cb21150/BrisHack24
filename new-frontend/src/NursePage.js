9e8b455765ae9c08c3e9654f3352607ffb56e89c
Chris-C3A <anon_c3a@outlook.com>
2024-02-26 09:33:25 +0000

    fixed bugs and added loading funcitonality when submitting

--- new-frontend/src/NursePage.js
+++ new-frontend/src/NursePage.js
@@ -36,7 +36,7 @@ const NursePage = () => {
 
   return (
     <div className="container mx-auto mt-8">
-      <h1 className="text-3xl font-bold mb-4">Subitted Patient's Conditions</h1>
+      <h1 className="text-3xl font-bold mb-4">Submitted Patient's Conditions</h1>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {patientData.map((patient) => (
